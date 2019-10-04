6783bbbe68d13c6a2fea6ab8da417817401736cf
Jonas Jerndin <swizkon@gmail.com>
2019-10-04 13:45:20 +0200

    Test with dev signing

--- Startup.cs
+++ Startup.cs
@@ -35,6 +35,7 @@ namespace IdentityServer
             else
             {
                 // throw new Exception("need to configure key material");
+                builder.AddDeveloperSigningCredential();
             }
         }
 
